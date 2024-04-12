26c0b241569d9e50d55553030e41a3267ac2bf0b
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-04-11 23:25:13 -0300

    resolução 01

--- try_except_if_elif_else/01_verificacao_de_qualidade_de_dados.py
+++ try_except_if_elif_else/01_verificacao_de_qualidade_de_dados.py
@@ -1,6 +1,13 @@
 ### Exercício 
 
-# Você está analisando um conjunto de dados de vendas e precisa garantir 
-# que todos os registros tenham valores positivos para `quantidade` e `preço`. 
-# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos 
-# forem positivos ou "Dados inválidos" caso contrário.
+# Você está analisando um conjunto de dados de vendas e precisa garantir que todos os registros tenham valores positivos para `quantidade` e `preço`. 
+# Escreva um programa que verifique esses campos e imprima "Dados válidos" se ambos forem positivos ou "Dados inválidos" caso contrário.
+
+quantidade = int(input("Digite a quantidade: \n"))
+preço = int(input("Digite o preço: \n"))
+
+if quantidade > 0 and preço > 0:
+    print("Dados válidos")
+
+else:
+    print("Dados inválidos")
