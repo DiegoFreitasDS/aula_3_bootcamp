413881fe2f291be0d1434c5181919f53cf8ebcdd
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-04-13 01:15:41 -0300

    Resolução 09

--- try_except_if_elif_else/09_extracao_de_subconjuntos_de_dados.py
+++ try_except_if_elif_else/09_extracao_de_subconjuntos_de_dados.py
@@ -1,2 +1,14 @@
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+
+numeros = range(1, 11)
+
+for i in numeros:
+    if i % 2 == 0:
+        print(i)
+
+
+# numeros = range(1, 11)
+# pares = [x for x in numeros if x % 2 == 0]
+
+# print(pares)
